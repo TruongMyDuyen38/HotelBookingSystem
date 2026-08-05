722b7bdeaedc41ff8b33f9dea25e39b6d91ff3ac
Trương Mỹ Duyên <duyentruong.25082019@gmail.com>
2026-08-05 15:55:45 +0700

    Hoàn thành chức năng quản lý loại phòng (CRUD)

---
+++ HotelBookingSystem/Controllers/LoaiPhongController.cs
@@ -0,0 +1,90 @@
+﻿using HotelBookingSystem.Data;
+using HotelBookingSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBookingSystem.Controllers
+{
+    public class LoaiPhongController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LoaiPhongController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var dsLoaiPhong = _context.LoaiPhongs.ToList();
+
+            return View(dsLoaiPhong);
+        }
+        //GET: /LoaiPhong/Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //POST: /LoaiPhong/Create
+        [HttpPost]
+        public IActionResult Create(LoaiPhong model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            _context.LoaiPhongs.Add(model);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //GET: /LoaiPhong/Edit
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var loaiPhong = _context.LoaiPhongs.Find(id);
+            if (loaiPhong == null)
+            {
+                return NotFound();
+            }
+            return View(loaiPhong);
+        }
+        //POST: /LoaiPhong/Edit
+        [HttpPost]
+        public IActionResult Edit(LoaiPhong model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            _context.LoaiPhongs.Update(model);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //GET: /LoaiPhong/Delete
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var loaiPhong = _context.LoaiPhongs.Find(id);
+            if (loaiPhong == null)
+            {
+                return NotFound();
+            }
+            return View(loaiPhong);
+        }
+        //POST: /LoaiPhong/Delete
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var loaiPhong = _context.LoaiPhongs.Find(id);
+
+            if (loaiPhong == null)
+            {
+                return NotFound();
+            }
+
+            _context.LoaiPhongs.Remove(loaiPhong);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ HotelBookingSystem/Views/LoaiPhong/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HotelBookingSystem.Views.LoaiPhong
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
