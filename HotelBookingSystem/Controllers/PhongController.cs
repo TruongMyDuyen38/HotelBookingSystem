67652bacc203bce39b95eef0ef9dac45044d1e10
Trương Mỹ Duyên <duyentruong.25082019@gmail.com>
2026-08-07 21:18:39 +0700

    Hoàn thành chức năng quản lý hình ảnh phòng

--- HotelBookingSystem/Controllers/PhongController.cs
+++ HotelBookingSystem/Controllers/PhongController.cs
@@ -1,5 +1,6 @@
 ﻿using HotelBookingSystem.Data;
 using HotelBookingSystem.Models;
+using HotelBookingSystem.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -9,10 +10,11 @@ namespace HotelBookingSystem.Controllers
     public class PhongController : Controller
     {
         private readonly ApplicationDbContext _context;
-
-        public PhongController(ApplicationDbContext context)
+        private readonly IWebHostEnvironment _environment;
+        public PhongController(ApplicationDbContext context,IWebHostEnvironment environment)
         {
             _context = context;
+            _environment = environment;
         }
         public IActionResult Index()
         {
@@ -138,7 +140,121 @@ namespace HotelBookingSystem.Controllers
                 return NotFound();
             }
 
-            return View(phong);
+            var dsAnh = _context.HinhAnhPhongs
+                .Where(h => h.MaPhong == id)
+                .ToList();
+
+            var viewModel = new PhongHinhAnhViewModel
+            {
+                Phong = phong,
+                DanhSachAnh = dsAnh
+            };
+
+            return View(viewModel);
+        }
+        //GET: /Images
+        [HttpGet]
+        public IActionResult Images(int id)
+        {
+            var phong = _context.Phongs
+                .Include(p => p.LoaiPhong)
+                .FirstOrDefault(p => p.MaPhong == id);
+
+            if (phong == null)
+            {
+                return NotFound();
+            }
+
+            var dsAnh = _context.HinhAnhPhongs
+                .Where(h => h.MaPhong == id)
+                .ToList();
+
+            var viewModel = new PhongHinhAnhViewModel
+            {
+                Phong = phong,
+                DanhSachAnh = dsAnh
+            };
+
+            return View(viewModel);
+        }
+        [HttpPost]
+        public IActionResult UploadImage(int id, IFormFile imageFile)
+        {
+            // 1. Kiểm tra file
+            if (imageFile == null || imageFile.Length == 0)
+            {
+                return RedirectToAction("Images", new { id });
+            }
+
+            // 2. Tạo đường dẫn thư mục
+            var folderPath = Path.Combine(
+                _environment.WebRootPath,
+                "images",
+                "rooms"
+            );
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+            var fileName = Guid.NewGuid().ToString() +
+               Path.GetExtension(imageFile.FileName);
+
+            var filePath = Path.Combine(folderPath, fileName);
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                imageFile.CopyTo(stream);
+            }
+            var hinhAnh = new HinhAnhPhong
+            {
+                MaPhong = id,
+                DuongDanAnh = "/images/rooms/" + fileName
+            };
+
+            _context.HinhAnhPhongs.Add(hinhAnh);
+            _context.SaveChanges();
+
+            return RedirectToAction("Images", new { id });
+        }
+        //GET: /Phong/DeleteImage
+        [HttpGet]
+        public IActionResult DeleteImage(int id)
+        {
+            var hinhAnh = _context.HinhAnhPhongs.Find(id);
+
+            if (hinhAnh == null)
+            {
+                return NotFound();
+            }
+
+            return View(hinhAnh);
+        }
+        //POST: /Phong/DeleteImage
+        [HttpPost, ActionName("DeleteImage")]
+        public IActionResult DeleteImageConfirmed(int maHinhAnh)
+        {
+            var hinhAnh = _context.HinhAnhPhongs.Find(maHinhAnh);
+
+            if (hinhAnh == null)
+            {
+                return NotFound();
+            }
+
+            var filePath = Path.Combine(
+                _environment.WebRootPath,
+                hinhAnh.DuongDanAnh
+                    .TrimStart('/')
+                    .Replace("/", Path.DirectorySeparatorChar.ToString())
+            );
+
+            if (System.IO.File.Exists(filePath))
+            {
+                System.IO.File.Delete(filePath);
+            }
+
+            _context.HinhAnhPhongs.Remove(hinhAnh);
+            _context.SaveChanges();
+
+            return RedirectToAction("Images", new { id = hinhAnh.MaPhong });
         }
     }
 }
---
+++ HotelBookingSystem/Models/ViewModels/PhongHinhAnhViewModel.cs
@@ -0,0 +1,11 @@
+﻿using HotelBookingSystem.Models;
+
+namespace HotelBookingSystem.Models.ViewModels
+{
+    public class PhongHinhAnhViewModel
+    {
+        public Phong Phong { get; set; } = null!;
+
+        public List<HinhAnhPhong> DanhSachAnh { get; set; } = new();
+    }
+}
