32f6db45c1d659a84ac96ccf9172cc6862567bc9
Trương Mỹ Duyên <duyentruong.25082019@gmail.com>
2026-08-16 21:26:49 +0700

    Hoàn thiện quản lý khách hàng

---
+++ HotelBookingSystem/Controllers/KhachHangController.cs
@@ -0,0 +1,47 @@
+﻿using HotelBookingSystem.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBookingSystem.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class KhachHangController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public KhachHangController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /KhachHang
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var danhSachKhachHang = _context.KhachHangs
+                .Include(k => k.TaiKhoan)
+                .OrderBy(k => k.MaKhachHang)
+                .ToList();
+
+            return View(danhSachKhachHang);
+        }
+
+        // GET: /KhachHang/Details/5
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var khachHang = _context.KhachHangs
+                .Include(k => k.TaiKhoan)
+                .Include(k => k.DatPhongs)
+                .FirstOrDefault(k => k.MaKhachHang == id);
+
+            if (khachHang == null)
+            {
+                return NotFound();
+            }
+
+            return View(khachHang);
+        }
+    }
+}
---
+++ HotelBookingSystem/Views/KhachHang/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HotelBookingSystem.Views.KhachHang
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
